529f3977871e422c9971ea51469dff33452df54b
nmcb <marco@splatter.nl>
2021-11-16 16:17:54 +0100

    initial port

---
+++ src/main/scala/alga.scala
@@ -0,0 +1,62 @@
+package alga
+
+enum Graph[A]:
+  case Empty                                 extends Graph[Nothing]
+  case Vertex(a: A)                          extends Graph[A]
+  case Overlay(lhs: Graph[A], rhs: Graph[A]) extends Graph[A]
+  case Connect(lhs: Graph[A], rhs: Graph[A]) extends Graph[A]
+
+  def +(that: Graph[A]): Graph[A] =
+    Overlay(this, that)
+
+  def *(that: Graph[A]): Graph[A] =
+    Connect(this, that)
+
+  def foldg[B](e: B, v: A => B, o: (B, B) => B, c: (B, B) => B): B =
+    this match
+      case Empty         => e
+      case Vertex(a)     => v(a)
+      case Overlay(l, r) => o(l.foldg(e, v, o, c), r.foldg(e, v, o, c))
+      case Connect(l, r) => c(l.foldg(e, v, o, c), r.foldg(e, v, o, c))
+
+  def map[B](f: A => B): Graph[B] =
+    foldg(Graph.empty, f.andThen(Vertex[B]), Overlay[B], Connect[B])
+
+  def flatMap[B](f: A => Graph[B]): Graph[B] =
+    foldg(Graph.empty, f, Overlay[B], Connect[B])
+
+  def isEmpty: Boolean =
+    foldg(true, _ => false, _ && _, _ && _)
+
+  def size: Int =
+    foldg(1, _ => 1, _ + _, _ + _)
+
+  def vertexSet: Set[A] =
+    foldg(Set.empty, Set(_), _ ++ _, _ ++ _)
+
+  def edgeSet: Set[(A, A)] =
+    this match
+      case Empty         => Set.empty
+      case Vertex(_)     => Set.empty
+      case Overlay(l, r) => l.edgeSet ++ r.edgeSet
+      case Connect(l, r) => l.edgeSet ++ r.edgeSet ++ l.vertexSet.flatMap(a => r.vertexSet.map(b => (a, b)))
+
+  def ===(that: Graph[A]): Boolean =
+    this.vertexSet == that.vertexSet && this.edgeSet == that.edgeSet
+
+  def =/=(that: Graph[A]): Boolean =
+    ! ===(that)
+
+object Graph:
+
+  def empty[A]: Graph[A] =
+    Empty.asInstanceOf[Graph[A]]
+
+  extension (i: Int) def node: Graph[Int] =
+    Vertex(i)
+
+@main def run =
+  import Graph.*
+  val lhs = (1.node + 2.node) + 3.node
+  val rhs = 1.node + (2.node + 3.node)
+  assert(lhs === rhs)
